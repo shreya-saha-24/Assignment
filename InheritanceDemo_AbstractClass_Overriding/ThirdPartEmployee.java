397cc3bd2b0be9ced7e88b91c0cc78c3ef6c47a0
shreya-saha-24 <159118207+shreya-saha-24@users.noreply.github.com>
2024-10-17 20:51:18 +0530

    Create  ThirdPartEmployee.java

---
+++ InheritanceDemo_AbstractClass_Overriding/ThirdPartEmployee.java
@@ -0,0 +1,13 @@
+package p1;
+
+public class ThirdPartEmployee extends Employee {
+
+
+	@Override
+	public void creditSalary() {
+		// TODO Auto-generated method stub
+		System.out.println("Not credited from organisation");
+	}
+	
+
+}
